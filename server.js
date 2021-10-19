f1ca0c76f519bfdff06ef5413070170daef81131
Luke Michalakis <michalakisluke@gmail.com>
2021-10-19 11:49:16 -0400

    <Build database functionality>

--- server.js
+++ server.js
@@ -1,10 +1,28 @@
 const express = require('express');
 const PORT = process.env.PORT || 3001;
 const app = express();
+const mysql = require('mysql2');
 
 // Express middleware
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+
+// Connect to database w/ verification
+const db = mysql.createConnection(
+    {
+        host: 'localhost',
+        // Your MySQL username
+        user: 'root',
+        // Your MySQL password
+        password: 'n2fCs4rvhp!',
+        database: 'election'
+    },
+    console.log('Connected to the election database.')
+);
+
+db.query(`SELECT * FROM candidates`, (err, rows) => {
+    console.log(rows);
+});
 
 // Default response for any other request (Not found)
 // Must be immeditaley before app.listen
